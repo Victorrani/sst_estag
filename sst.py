815d2014e4a668d6489ac0af4968b34d092e64af
Victorrani <victor.ranieri90@gmail.com>
2025-04-05 09:20:59 -0300

    script_principal

--- sst.py
+++ sst.py
@@ -1,11 +1,12 @@
-
-
-
 import sys
 import time
+import os
 
-DIR_SCRIPT = "/p1-chima/victor/sst_estag/scripts/"
-sys.path.append(DIR_SCRIPT)  # Adiciona ao sys.path
+
+DIR_BASE = os.getcwd()
+DIR_SCRIPTS = os.path.join(DIR_BASE, 'scripts')
+
+sys.path.append(DIR_SCRIPTS)  # Adiciona ao sys.path
 import sst_download
 
 inicio = time.time()  # Marca o tempo de início
@@ -15,4 +16,4 @@ sst_download.download_sst_data()
 fim = time.time()  # Marca o tempo de fim
 
 tempo_total = fim - inicio
-print(f"Tempo de execução: {tempo_total:.2f} segundos")
+print(f"Tempo de execução total do script: {tempo_total:.2f} segundos")
